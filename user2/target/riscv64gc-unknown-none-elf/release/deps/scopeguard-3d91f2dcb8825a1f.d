8d99b1ebc159fb397c522ff5848ad2807c0853ae
ChampDeLin <kondl@DESKTOP-OBNAKTV.localdomain>
2022-09-01 23:06:18 +0800

    update

---
+++ user2/target/riscv64gc-unknown-none-elf/release/deps/scopeguard-3d91f2dcb8825a1f.d
@@ -0,0 +1,7 @@
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/scopeguard-3d91f2dcb8825a1f.rmeta: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/scopeguard-1.1.0/src/lib.rs
+
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/libscopeguard-3d91f2dcb8825a1f.rlib: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/scopeguard-1.1.0/src/lib.rs
+
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/scopeguard-3d91f2dcb8825a1f.d: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/scopeguard-1.1.0/src/lib.rs
+
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/scopeguard-1.1.0/src/lib.rs:
---
+++ user2/target/riscv64gc-unknown-none-elf/release/deps/spin-1016aecbc56adf91.d
@@ -0,0 +1,10 @@
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/spin-1016aecbc56adf91.rmeta: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs
+
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/libspin-1016aecbc56adf91.rlib: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs
+
+/home/kondl/Os/lab0-1-run-os2-ChampDeLin/user2/target/riscv64gc-unknown-none-elf/release/deps/spin-1016aecbc56adf91.d: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs
+
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/lib.rs:
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/mutex.rs:
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/rw_lock.rs:
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/spin-0.5.2/src/once.rs:
